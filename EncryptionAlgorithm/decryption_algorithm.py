57d087a57f191a796c3099fa15ee3287b34fcfbe
Ruth Wager <64973289+ruth-wager@users.noreply.github.com>
2023-04-16 18:08:46 +0200

    Fixed encryption algorithm and changed decryption algorithm to match
    Fixed the encryption algorithm that was messing up how to assign characters to an image file, and also changed the decryption file accordingly

--- EncryptionAlgorithm/decryption_algorithm.py
+++ EncryptionAlgorithm/decryption_algorithm.py
@@ -10,17 +10,25 @@ transparency= list(enIm.getdata(band=3))
 
 enIm.close()
 
+#create a new textfile and put the decrypted text in it
 txtfile = open("decryptedText.txt", "w") 
 #create or overwrite the decryptedText.txt file 
 #-> https://www.w3schools.com/python/python_file_write.asp
 for r in red:
-    txtfile.write(chr(r))
+    character = chr(r)
+    if (character != 3): #if the character is not 3 (ASCII end of file)
+        txtfile.write(character)
 for g in green:
-    txtfile.write(chr(g))
+    character = chr(g)
+    if (character != 3): #if the character is not 3 (ASCII end of file)
+        txtfile.write(character)
 for b in blue:
-    txtfile.write(chr(b))
+    character = chr(b)
+    if (character != 3): #if the character is not 3 (ASCII end of file)
+        txtfile.write(character)
 for t in transparency:
-    txtfile.write(chr(t))
+    character = chr(t)
+    if (character != 3): #if the character is not 3 (ASCII end of file)
+        txtfile.write(character)
 # convert from int to ASCII: https://www.geeksforgeeks.org/python-ways-to-convert-list-of-ascii-value-to-string/?ref=lbp
 txtfile.close()
-#-------------------------------------------
--- EncryptionAlgorithm/encryption_algorithm.py
+++ EncryptionAlgorithm/encryption_algorithm.py
@@ -7,60 +7,42 @@ numSquares = 4
 
 # choose file & read it in (check to make sure it's the right type?)
 with open("example.txt") as file:
-    contents = file.read()#.lower()
+    contents = file.read()
     #print(contents)
     file.close()
-# change to all lowercase? .lower()^
-# size = Ceil(sqr(ST/4))
+
+# size = Ceil(sqr(ST/4)) --> determines how big each square is based on how many at least are needed to fit the entire text file
     ST = len(contents)
     #print(ST)
     STsize = math.ceil(math.sqrt(ST/numSquares))
     #print(STsize)
 
-#source array = (#images, #channels, x_dim, y_dim) #channels = 4 = RGBA/CYMK
-
-#from the research paper:"
-# By default, four keys were used in this paper, each layer has its own key, 
-    #and as previously mentioned, it is possible to use several keys according to the programmer's desire.
-    #Each letter takes a color, in CMYK this is a 32 bit number of four possible colors with 256 levels each.
-    #The first key is for Layer 1 (cyan), the second key is for Layer 2 (magenta),
-    #the third key is for Layer 3 (yellow), and the fourth key is for Layer 4 (black). 
-    #All keys are entered by the sender according to the language to be encrypted: 
-    #English, Arabic, Hindi or any other language. This is one of the advantages of this work.
-    #It is also possible to encrypt a text consisting of letters in various languages. 
-    #In this work, four keys were used; each key was 37 characters long.
-#"
-
-#I chose not to implement the key part (which is what actually makes the encryption unique and therefore secure)
-
-
 red = np.full((STsize,STsize), 0)
 green = np.full((STsize,STsize), 0)
 blue = np.full((STsize, STsize),0)
-transparency = np.full((STsize, STsize),255)
+transparency = np.full((STsize, STsize),3)
 
 STsquare = STsize * STsize
 
 
 #https://www.geeksforgeeks.org/python-convert-string-list-to-ascii-values/ --> convert chars to their ascii equivalents
 
-outOfChars = False
+i = 0
 for z in range(numSquares): # z = 0 to 3
     for r in range(STsize): # r = 0 to STsize - 1
         for c in range(STsize): # c = 0 to STsize - 1
-            currentIndex = (z * STsize ** z) + (r * STsize) + c # number ** exponent = n^e
-            if currentIndex >= ST: outOfChars = True# if you've run out of characters in the input, just quit the loop since you can't access anything
-            if z == 0 and  not outOfChars: # z = 0, working with cyan
-                red[r][c] = ord(contents[currentIndex])
-            elif z == 1 and not outOfChars:
-                green[r][c] = ord(contents[currentIndex])
-            elif z == 2 and not outOfChars:
-                blue[r][c] = ord(contents[currentIndex])
-            elif z == 3 and not outOfChars :
-                transparency[r][c] = ord(contents[currentIndex])
-            elif not outOfChars:
-                print("Sorry, I expected more color layers than there were.") # ie z >= number of color layers (4 for cymk)
-
+            if i < ST: # if there's still characters left to assign
+                if z == 0:
+                    red[r][c] = ord(contents[i])
+                elif z == 1:
+                    green[r][c] = ord(contents[i])
+                elif z == 2:
+                    blue[r][c] = ord(contents[i])
+                elif z == 3:
+                    transparency[r][c] = ord(contents[i])
+                else:
+                    print("Apologies, I was expecting more image layers than there are.")
+                i += 1 # increment i
 
 
 rgba_values = []#creates a new array
@@ -79,7 +61,7 @@ for j in range(STsize):
 #im.save("encryptedImage.png")
 
 from PIL import Image
-img = Image.new("RGBA", (STsize, STsize), color=0)#creates a new image with RGBA format, since PIL plays nicer with RBGA than with CYMK
+img = Image.new("RGBA", (STsize, STsize), color=3)#creates a new image with RGBA format, since PIL plays nicer with RBGA than with CYMK
 img.putdata(rgba_values) #assigns each pixel color
 img.save("encryptedImage.png") #saves the image
 
